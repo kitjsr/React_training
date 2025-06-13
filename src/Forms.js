c113b15bd25755c026564a9834d74045166fd7a5
kitjsr <91665868+kitjsr@users.noreply.github.com>
2025-06-13 10:00:07 +0530

    Forms updated

--- src/Forms.js
+++ src/Forms.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Formik, Field, ErrorMessage } from 'formik';
+ 
+
 
 export const Forms = () => {
   return (
     <div>
-      <Form>
+      <Formik
+       initialValues={{ email: '', password: '',}}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         if (!values.password) {
+           errors.password = 'Required';
+         } else if (values.password.length > 8 && values.password.length < 20)
+        {
+           errors.password= 'Must be 20 characters or less';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+      {({ isSubmitting,values }) => (
+            <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
@@ -13,49 +43,26 @@ export const Forms = () => {
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+      {/* <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Example textarea</Form.Label>
         <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <>
-      <Form.Control size="lg" type="text" placeholder="Large text" />
-      <br />
-      <Form.Control type="text" placeholder="Normal text" />
-      <br />
-      <Form.Control size="sm" type="text" placeholder="Small text" />
-    </>
-      </Form.Group>
-      <Form.Group>
-        <>
-      <Form.Control
-        type="text"
-        placeholder="Disabled input"
-        aria-label="Disabled input example"
-        disabled
-        readOnly
-      />
-      <br />
-      <Form.Control
-        type="text"
-        placeholder="Disabled readonly input"
-        aria-label="Disabled input example"
-        readOnly
-      />
-    </>
       </Form.Group>
       
       <Form.Group controlId="formFileMultiple" className="mb-3">
         <Form.Label>Multiple files input example</Form.Label>
         <Form.Control type="file" multiple />
-      </Form.Group>
+      </Form.Group> */}
       <Form.Group>
         <>
       <Form.Label htmlFor="inputPassword5">Password</Form.Label>
       <Form.Control
         type="password"
+        name="password"
         id="inputPassword5"
         aria-describedby="passwordHelpBlock"
+        
+         onChange={formik.handleChange}
+         value={formik.values.lastName}
       />
       <Form.Text id="passwordHelpBlock" muted>
         Your password must be 8-20 characters long, contain letters and numbers,
@@ -63,37 +70,19 @@ export const Forms = () => {
       </Form.Text>
     </>
       </Form.Group>
-      <Form.Group>
-        <>
-      <Form.Select size="lg">
-        <option>Large select</option>
-      </Form.Select>
-      <br />
-      <Form.Select>
-        <option>Default select</option>
-      </Form.Select>
-      <br />
-      <Form.Select size="sm">
-        <option>Small select</option>
-      </Form.Select>
-    </>
-      </Form.Group>
-      <Form.Group>
-        <>
-      <Form.Label>Range</Form.Label>
-      <Form.Range />
-    </>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Form.Group>
-        
-      </Form.Group>
-      <Button variant="primary" type="submit">
+      {/* <Form.Group className="mb-3" controlId="formBasicCheckbox"><label>
+            <Field type="checkbox" name="toggle" />
+            {`${values.toggle}`}
+          </label>
+      </Form.Group> */}
+      <Button variant="primary" type="submit" disabled={isSubmitting}>
         Submit
       </Button>
     </Form>
+        
+       )}
+     </Formik>
+      
     </div>
   )
 }
